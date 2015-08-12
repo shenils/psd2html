32738b8b66fd042d9630f954fedcac517b34c951
shenils <shenil@bu.edu>
2015-08-12 09:52:18 +0530

    minify

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,21 +3,37 @@ module.exports = function(grunt) {
     sass: {                              // Task 
       dist: {                            // Target 
         files: {                         // Dictionary of files 
-          'styles.css': 'styles.scss'
+          'assets/stylesheets/common.css': 'assets/stylesheets/src/common.scss',
+          'assets/stylesheets/land.css': 'assets/stylesheets/src/land.scss'
         }
       }
     },
     watch: {
       css: {
-        files: ['*.scss'],
-        tasks: ['sass'],
+        files: ['assets/stylesheets/src/*.scss'],
+        tasks: ['sass', 'concat:css'],
       }
+    },
+    concat: {
+      css: {
+        src: ['assets/stylesheets/common.css', 'assets/stylesheets/land.css'],
+        dest: 'assets/stylesheets/app.css',
+      },
     }
+    // cssmin: {
+    //   target: {
+    //     files: {
+    //       'assets/stylesheets/app.min.css': ['assets/stylesheets/common.css', 'assets/stylesheets/land.css']
+    //     }
+    //   }
+    // }
   });
 
 
 
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.registerTask('default', ['sass', 'watch']);
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.registerTask('default', ['sass', 'concat:css', 'watch']);
 }
